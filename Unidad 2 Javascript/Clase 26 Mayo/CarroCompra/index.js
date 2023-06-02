2d4b6dc44ba0cc9ddfcbe557e459cfeccc28ca4c
Jorge <jorge.toledo.6@hotmail.com>
2023-06-02 11:53:01 -0400

    JS

--- Unidad 2 Javascript/Clase 26 Mayo/CarroCompra/index.js
+++ Unidad 2 Javascript/Clase 26 Mayo/CarroCompra/index.js
@@ -1,65 +1,80 @@
 const Productos = [
     {
-        "ProductoId" : 1,
-        "Descripcion" : "Sony PlayStation 5 (Blu-Ray / Glacier White)",
-        "Precio" : "529990",
-        "UrlImagen" : "https://media.solotodo.com/media/products/1182943_picture_1602146793.webp",
-        "Stock" : "5"
+        "ProductoId": 1,
+        "Descripcion": "Sony PlayStation 5 (Blu-Ray / Glacier White)",
+        "Precio": "529990",
+        "UrlImagen": "https://media.solotodo.com/media/products/1182943_picture_1602146793.webp",
+        "Stock": "5"
     },
     {
-        "ProductoId" : 2,
-        "Descripcion" : "Master-G MGMG2410",
-        "Precio" : "129990",
-        "UrlImagen" : "https://media.solotodo.com/media/products/1763241_picture_1684478552.webp",
-        "Stock" : "10"
+        "ProductoId": 2,
+        "Descripcion": "Master-G MGMG2410",
+        "Precio": "129990",
+        "UrlImagen": "https://media.solotodo.com/media/products/1763241_picture_1684478552.webp",
+        "Stock": "10"
     },
     {
-        "ProductoId" : 3,
-        "Descripcion" : "Apple iPhone 14 Pro (128 GB / Space Black)",
-        "Precio" : "799990",
-        "UrlImagen" : "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
-        "Stock" : "3"
+        "ProductoId": 3,
+        "Descripcion": "Apple iPhone 14 Pro (128 GB / Space Black)",
+        "Precio": "799990",
+        "UrlImagen": "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
+        "Stock": "3"
     },
     {
-        "ProductoId" : 3,
-        "Descripcion" : "Apple iPhone 14 Pro (128 GB / Space Black)",
-        "Precio" : "799990",
-        "UrlImagen" : "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
-        "Stock" : "3"
-    },
-    {
-        "ProductoId" : 3,
-        "Descripcion" : "Apple iPhone 14 Pro (128 GB / Space Black)",
-        "Precio" : "799990",
-        "UrlImagen" : "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
-        "Stock" : "3"
-    },
-    {
-        "ProductoId" : 3,
-        "Descripcion" : "Apple iPhone 14 Pro (128 GB / Space Black)",
-        "Precio" : "799990",
-        "UrlImagen" : "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
-        "Stock" : "3"
+        "ProductoId": 3,
+        "Descripcion": "Apple iPhone 14 Pro (128 GB / Space Black)",
+        "Precio": "799990",
+        "UrlImagen": "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
+        "Stock": "3"
+    }, {
+        "ProductoId": 3,
+        "Descripcion": "Apple iPhone 14 Pro (128 GB / Space Black)",
+        "Precio": "799990",
+        "UrlImagen": "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
+        "Stock": "3"
+    }, {
+        "ProductoId": 3,
+        "Descripcion": "Apple iPhone 14 Pro (128 GB / Space Black)",
+        "Precio": "799990",
+        "UrlImagen": "https://media.solotodo.com/media/products/1641307_picture_1662845386.webp",
+        "Stock": "3"
     }
 
 
 ]
-
-
-
-const CLFormat = new Intl.NumberFormat('es-CL',{
-    style: 'currency',
-    currency:'CLP'
-})
-
+//console.log(localStorage.getItem('Productos'))
+// let productosFromLocalStorage
+// if(localStorage.getItem('Productos') == null){
+//     localStorage.setItem('Productos', JSON.stringify(Productos))
+// }else{
+//     productosFromLocalStorage = localStorage.getItem('Productos')
+// }
+const productContainer = document.getElementById('productContainer')
 const btnMostrarCarro = document.getElementById("MostrarCarro")
 const btnCerrarCarro = document.getElementById("CerrarCarro")
 const carroContainer = document.getElementById("carroContainer")
 const productCarroContainer = document.getElementById('productCarroContainer')
+const CLFormat = new Intl.NumberFormat('es-CL', {
+    style: 'currency',
+    currency: 'CLP'
+})
+let CarroCompras = []
+if(localStorage.getItem('CarroCompras') == null){
+    localStorage.setItem('CarroCompras', JSON.stringify(CarroCompras))
+}
+CarroCompras = JSON.parse(localStorage.getItem('CarroCompras'))
+console.log(CarroCompras)
+cargarCarro()
 
 
 
-const loadProducts = (products) =>{
+
+
+
+
+
+
+const loadProducts = (products) => {
 
     products.forEach(P => {
         const card = document.createElement('div')
@@ -89,62 +104,11 @@ const loadProducts = (products) =>{
         btnAdd.innerHTML = '<i class="fa-solid fa-cart-plus"></i>'
 
         btnAdd.addEventListener('click', ()=>{
-          const divCarro = document.createElement('div')
-          divCarro.classList.add('my-2', 'mx-5','p-3', 'text-center', 'flex', 'custom-card')
-
-        const divDesc = document.createElement('div')
-        divDesc.style.minWidth = "50%"
-
-          const descCarro = document.createElement('p')
-          descCarro.classList.add('card-title')
-          descCarro.innerText = P.Descripcion
-
-          const imgCarro = document.createElement('img')
-          imgCarro.classList.add('mt-2', 'd-block', 'mx-auto')
-          imgCarro.style.width = "100px"
-          imgCarro.style.minHeight = "100px"
-          imgCarro.style.maxHeight = "100px"
-          imgCarro.src = P.UrlImagen
-
-          const cantidadCarro = document.createElement('p')
-          cantidadCarro.classList.add('card-title')
-          cantidadCarro.innerText = 'Cantidad: '
-          const spanCantidad = document.createElement('span')
-          spanCantidad.innerText = 1
-          cantidadCarro.appendChild(spanCantidad)
-
-          const precioCarro = document.createElement('p')
-          precioCarro.classList.add('card-title')
-          precioCarro.innerText = 'Precio Unitario: ' + CLFormat.format(P.Precio)
-
-          const subTotalCarro = document.createElement('p')
-          subTotalCarro.classList.add('card-title')
-          subTotalCarro.innerText = 'Subtotal: '
-          const valorSubTotalCarro = document.createElement('span')
-          valorSubTotalCarro.innerText = CLFormat.format(P.Precio)
-          subTotalCarro.appendChild(valorSubTotalCarro)
-
-
-          const btnBorrar = document.createElement('button')
-          btnBorrar.innerHTML = '<i class="fa-solid fa-trash"></i>'
-          btnBorrar.classList.add('btnBorrar', 'btn')
-          btnBorrar.addEventListener('click', ()=> {
-            productCarroContainer.removeChild(divCarro)
-            })
-
-            
-            divCarro.appendChild(imgCarro)
-            divDesc.appendChild(descCarro)
-            divDesc.appendChild(precioCarro)
-            divDesc.appendChild(cantidadCarro)
-            divDesc.appendChild(subTotalCarro)
-            divDesc.appendChild(btnBorrar)
-            divCarro.appendChild(divDesc)
-
-            productCarroContainer.appendChild(divCarro)
-            
-
+            CarroCompras.push(P)
+            localStorage.setItem('CarroCompras', JSON.stringify(CarroCompras))
+            cargarCarro()
         })
+            
 
 
         card.appendChild(h2)
@@ -152,28 +116,86 @@ const loadProducts = (products) =>{
         card.appendChild(precio)
         card.appendChild(btnAdd)
 
-        
+
         productContainer.appendChild(card)
 
     });
 
-}
-
+} 
 
 loadProducts(Productos)
 
 
-
-btnMostrarCarro.addEventListener('click' , () =>{
+btnMostrarCarro.addEventListener('click', () => {
     carroContainer.style.height = '90vh'
     btnCerrarCarro.style.display = 'inline'
     productCarroContainer.style.display = 'block'
 })
-btnCerrarCarro.addEventListener('click' , () =>{
+btnCerrarCarro.addEventListener('click', () => {
     carroContainer.style.height = '0vh'
     btnCerrarCarro.style.display = 'none'
     productCarroContainer.style.display = 'none'
 })
 
+function cargarCarro(){
+    productCarroContainer.innerHTML = ''
+    CarroCompras.forEach(P =>{
+        const divCarro = document.createElement('div')
+        divCarro.classList.add('my-2', 'mx-5', 'p-3', 'text-center', 'flex', 'custom-card')
+
+        const divDesc = document.createElement('div')
+        divDesc.style.minWidth = "50%"
+
+        const descCarro = document.createElement('p')
+        descCarro.classList.add('card-title')
+        descCarro.innerText = P.Descripcion
+
+        const imgCarro = document.createElement('img')
+        imgCarro.classList.add('mt-2', 'd-block', 'mx-auto')
+        imgCarro.style.width = "100px"
+        imgCarro.style.minHeight = "100px"
+        imgCarro.style.maxHeight = "100px"
+        imgCarro.src = P.UrlImagen
+
+        const cantidadCarro = document.createElement('p')
+        cantidadCarro.classList.add('card-title')
+        cantidadCarro.innerText = 'Cantidad: '
+        const spanCantidad = document.createElement('span')
+        spanCantidad.innerText = 1
+        cantidadCarro.appendChild(spanCantidad)
+
+        const precioCarro = document.createElement('p')
+        precioCarro.classList.add('card-title')
+        precioCarro.innerText = 'Precio Unitario: ' + CLFormat.format(P.Precio)
+
+        const subTotalCarro = document.createElement('p')
+        subTotalCarro.classList.add('card-title')
+        subTotalCarro.innerText = 'Subtotal: '
+        const valorSubTotalCarro = document.createElement('span')
+        valorSubTotalCarro.innerText = CLFormat.format(P.Precio)
+        subTotalCarro.appendChild(valorSubTotalCarro)
 
 
+        const btnBorrar = document.createElement('button')
+        btnBorrar.innerHTML = '<i class="fa-solid fa-trash"></i>'
+        btnBorrar.classList.add('btnBorrar', 'btn')
+        btnBorrar.addEventListener('click', () => {
+            productCarroContainer.removeChild(divCarro)
+        })
+
+
+        divCarro.appendChild(imgCarro)
+        divDesc.appendChild(descCarro)
+        divDesc.appendChild(precioCarro)
+        divDesc.appendChild(cantidadCarro)
+        divDesc.appendChild(subTotalCarro)
+        divDesc.appendChild(btnBorrar)
+        divCarro.appendChild(divDesc)
+
+        productCarroContainer.appendChild(divCarro)
+
+
+    })
+    
+
+}

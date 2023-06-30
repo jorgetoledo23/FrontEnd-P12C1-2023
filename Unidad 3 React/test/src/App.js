33c9281bed282fdc0024b212e24bd26a10dbd1eb
Jorge <jorge.toledo.6@hotmail.com>
2023-06-30 09:14:25 -0400

    Carro

--- Unidad 3 React/test/src/App.js
+++ Unidad 3 React/test/src/App.js
@@ -3,24 +3,25 @@ import './App.css';
 import Navbar from './componentes/Navbar';
 import Card from './componentes/Card'
 import axios from 'axios'
+import data from './data.json'
 export default function App() {
 
 
-    const [productos, setProductos] = React.useState([])
+    const [productos, setProductos] = React.useState(data)
 
 
-    React.useEffect(() => {
-      axios.get('https://localhost:7038/api/Products/getProducts').then((response) => {
-        setProductos(response.data);
-      });
-    }, []);
+    // React.useEffect(() => {
+    //   axios.get('https://localhost:7038/api/Products/getProducts').then((response) => {
+    //     setProductos(response.data);
+    //   });
+    // }, []);
 
 
    
     return (
         <div className="App">
             <Navbar/>
-            <div className='row'>
+            <div className='row justify-content-evenly'>
                 {
                 productos.map(x => <Card producto={x}/>)
             } </div>
--- Unidad 3 React/test/src/componentes/Card.jsx
+++ Unidad 3 React/test/src/componentes/Card.jsx
@@ -1,12 +1,18 @@
 export default function Card({ producto }) {
+    
+    const CLFormat = new Intl.NumberFormat('es-CL', {
+        style: 'currency',
+        currency: 'CLP'
+    })
+    
     return (
-        <div className="card col-md-3">
+        <div className="card col-md-3 m-2 p-2">
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <img src={producto.imageUrl} className="img-thumbnail"></img>
-                <p className="card-text">{producto.name}</p>
-                <p className="card-text">{producto.precio}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{producto.Descripcion}</h5>
+                <img width={150} src={producto.UrlImagen} className="img-thumbnail"></img>
+                <p className="card-text">Stock : {producto.Stock}</p>
+                <p className="card-text">Valor : {CLFormat.format(producto.Precio)}</p>
+                <button className="btn btn-primary">Add</button>
             </div>
         </div>
     )
